e2f71b52cf23513dc15f6e876ac04b37db97210f
drozdovas <dmitrijus.drozdovas@gmail.com>
2021-06-01 22:01:56 +0300

    Added user service and its template implementation

---
+++ src/main/java/com/drozdovas/conference/service/UserService.java
@@ -0,0 +1,7 @@
+package com.drozdovas.conference.service;
+
+import com.drozdovas.conference.model.User;
+
+public interface UserService {
+    User save(User user);
+}
---
+++ src/main/java/com/drozdovas/conference/service/UserServiceImpl.java
@@ -0,0 +1,13 @@
+package com.drozdovas.conference.service;
+
+import com.drozdovas.conference.model.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public User save(User user){
+        return null;
+    }
+}
